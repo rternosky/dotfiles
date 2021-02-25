419cb8432244c0fbc00a84bbecea09122ddde14e
Bob Ternosky <rternosky@navisite.com>
2021-02-25 13:43:31 -0500

    * add comment to login.sql

--- login.sql
+++ login.sql
@@ -1,3 +1,4 @@
+-- Put this file wherever you extract sqlcl: sqlcl/login.sql
 set sqlprompt "@|red _USER@_CONNECT_IDENTIFIER> |@"
 ALTER SESSION SET NLS_DATE_FORMAT='DD-MON-YYYY HH24:MI:SS'; 
 ALTER SESSION SET NLS_TIMESTAMP_FORMAT='DD-MON-YYYY HH24:MI:SS.FF'; 
